4417237ad41b93429c5dcf1ff81ca1d32a72eb0a
mangelov <mikhail.angelov@gmail.com>
2015-03-01 18:57:05 +0300

    add some classes to welcome page

--- app/components/welcome/welcome.js
+++ app/components/welcome/welcome.js
@@ -1,10 +1,15 @@
 'use strict';
 
 angular.module('bconfApp')
-  .controller('WelcomeController', function ($scope) {
+  .controller('WelcomeController', function ($scope, $location) {
     $scope.authProviders = [
-      {redirect: '/auth/facebook', icon: 'styles/icons/set/facebook fb social social media.svg', color: 'color:#00f;'},
-      {redirect: '/auth/google', icon: 'styles/icons/set/google plus.svg', color: 'color:#f00;'},
-      {redirect: '/auth/yandex', icon: 'styles/icons/set/yandex.svg', color: 'color:#fd2;'}
+      {redirect: '/auth/facebook', icon: 'styles/icons/set/facebook fb social social media.svg', class: 'blue'},
+      {redirect: '/auth/google', icon: 'styles/icons/set/google plus.svg', class: 'red'},
+      {redirect: '/auth/yandex', icon: 'styles/icons/set/yandex.svg', class: 'yellow'}
     ];
+
+    $scope.onLogin = function(index){
+      //$location.path( $scope.authProviders[index].redirect)
+console.log(index);
+    }
   });
