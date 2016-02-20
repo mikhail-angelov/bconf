c0840aa5d17b879dcdd36c09b9c2184519db428f
Mikhail Angelov <mikhail.angelov@gmail.com>
2016-02-20 21:21:57 +0300

    add travice build tasks 5

--- server/config/environment/test.js
+++ server/config/environment/test.js
@@ -5,7 +5,7 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost/bconf-test'
+    uri: 'mongodb://127.0.0.1/bconf-test'
   },
   sequelize: {
     uri: 'sqlite://',
