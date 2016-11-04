d2b00f988acab16b27e2a0fb634d40d9eccacd78
Mikhail Angelov <mikhail.angelov@gmail.com>
2016-11-04 10:22:53 +0300

    refine styles, use syles webpack loader

--- gulp/webpack.config.js
+++ gulp/webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   output: {
     path: __dirname + '/ui/dist',
     filename: 'bundle.js',
-    publicPath: 'http://0.0.0.0:8080/ui/dist/'
+    publicPath: 'http://0.0.0.0:8080/dist/'
   },
   plugins: [
     new webpack.ProvidePlugin({
@@ -17,7 +17,12 @@ module.exports = {
       { test: /\.tag$/, exclude: /node_modules/, loader: 'riotjs-loader', query: { type: 'none' } }
     ],
     loaders: [
-      { test: /\.js$|\.tag$/, exclude: /node_modules/, loader: 'babel-loader' }
+      { test: /\.js$|\.tag$/, exclude: /node_modules/, loader: 'babel-loader' },
+      { test: /\.css$/, loader: "style-loader!css-loader" },
+      { test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?prefix=imglimit=10000&mimetype=application/octet-stream'},
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
     ]
   },
   devServer: {
