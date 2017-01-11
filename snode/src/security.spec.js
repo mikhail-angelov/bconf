cfff5fd66613aa205374d1afed592c56cdd36b66
Mikhail Angelov <mikhail.angelov@gmail.com>
2017-01-11 17:25:23 +0300

    add ut for sequrity component

---
+++ snode/src/security.spec.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const expect = require('chai').expect
+
+describe('sequrity', () => {
+    const security = require('./security')
+    
+    it('should encode token', () => {
+        const token = 'test'
+        const encodedToken = security.encodeToken(token)
+        expect(token != encodedToken).to.equal(true)
+        expect(security.decodeToken(encodedToken)).to.equal(token)
+    })
+
+    it('should encode password', () => {
+        const password = 'test'
+        const encodedPassword = security.encodePassword(password)
+        // expect(token != encodedPassword).to.equal(true)
+        expect(security.validatePassword(password, encodedPassword)).to.equal(true)
+        expect(security.validatePassword('wrong', encodedPassword)).to.equal(false)
+    })
+
+})
