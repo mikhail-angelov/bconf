6960a82d4a9ae922195f5d69e0e237c2cc188885
Mikhail Angelov <mikhail.angelov@gmail.com>
2018-12-20 17:43:02 +0300

    add dev script for electron

--- desktop/index.js
+++ desktop/index.js
@@ -1,46 +1,61 @@
-'use strict'
-const electron = require('electron')
-const app = electron.app
+const electron = require('electron');
+// Module to control application life.
+const app = electron.app;
+// Module to create native browser window.
+const BrowserWindow = electron.BrowserWindow;
 
-// report crashes to the Electron project
-require('crash-reporter').start()
+const path = require('path');
+const url = require('url');
 
-// adds debug features like hotkeys for triggering dev tools and reload
-require('electron-debug')()
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow;
 
-// prevent window being garbage collected
-let mainWindow
+function createWindow() {
+    // Create the browser window.
+    mainWindow = new BrowserWindow({width: 800, height: 600});
 
-function onClosed () {
-  // dereference the window
-  // for multiple windows store them in an array
-  mainWindow = null
+    const startUrl = process.env.ELECTRON_START_URL || url.format({
+        pathname: path.join(__dirname, '/../client/build/index.html'),
+        protocol: 'file:',
+        slashes: true
+    });
+    // and load the index.html of the app.
+    mainWindow.loadURL('http://localhost:3000');
+
+    // Open the DevTools.
+    mainWindow.webContents.openDevTools();
+
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function () {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null
+    })
 }
 
-function createMainWindow () {
-  const win = new electron.BrowserWindow({
-    width: 600,
-    height: 400
-  })
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
 
-  win.loadURL(`file://${__dirname}/../frontend/index.html`)
-  win.on('closed', onClosed)
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+});
 
-  return win
-}
+app.on('activate', function () {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow()
+    }
+});
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-
-app.on('activate-with-no-open-windows', () => {
-  if (!mainWindow) {
-    mainWindow = createMainWindow()
-  }
-})
-
-app.on('ready', () => {
-  mainWindow = createMainWindow()
-})
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
