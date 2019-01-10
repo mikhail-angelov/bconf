18c5f7be60bd6dfea7534c0e8ef8124455d875fa
Olga Tsytserova <helgamir@gmail.com>
2019-01-10 14:46:42 +0300

    add login action; fix server url api; add error handling on login error on ui; add login data to localStorage;

--- client/src/index.js
+++ client/src/index.js
@@ -5,19 +5,23 @@ import Auth from './stores/auth'
 import Ui from './stores/ui'
 import Root from './components/Root'
 import './styles/index.css'
+import DevTools from 'mobx-react-devtools'
 
 // import registerServiceWorker from './registerServiceWorker'
 // import { unregister } from './registerServiceWorker';
 const stores = {
-  authStore: new Auth(),
-  uiStore: new Ui(),
+    authStore: new Auth(),
+    uiStore: new Ui(),
 }
 
 ReactDOM.render(
-  <Provider {...stores}>
-    <Root />
-  </Provider>,
-  document.getElementById('root')
+    <React.Fragment>
+        <DevTools />
+        <Provider {...stores}>
+            <Root />
+        </Provider>
+    </React.Fragment>,
+    document.getElementById('root')
 )
 // registerServiceWorker()
 // unregister()
--- client/src/stores/auth.js
+++ client/src/stores/auth.js
@@ -2,62 +2,71 @@ import { observable, flow, action } from 'mobx'
 import { LOGIN_URL, REGISTER_URL, doAuthRequest, setAuth, doJsonRequest } from './helper'
 
 export default class Auth {
-  @observable authenticated = false
-  @observable user = {}
-  @observable state = 'pending'
+    @observable authenticated = false
+    @observable user = {}
+    @observable state = 'pending'
 
-  login = flow(function*({ email, password }) {
-    this.state = 'pending'
-    try {
-      const data = yield doAuthRequest({
-        url: LOGIN_URL,
-        method: 'POST',
-        data: { email, password },
-      })
-      console.log('--', data)
-      this.authenticated = true
-      this.user = data.user
-      this.state = 'done'
-      setAuth(data)
-    } catch (err) {
-      console.log('auth error', err)
-      this.state = 'error'
+    login = flow(function*({ email, password }) {
+        this.state = 'pending'
+        try {
+            const data = yield doAuthRequest({
+                url: LOGIN_URL,
+                method: 'POST',
+                data: { email, password },
+            })
+            console.log('--', data)
+            this.authenticated = true
+            this.user = data.user
+            this.state = 'done'
+            localStorage.setItem('bconf', JSON.stringify({ user: data.user, token: data.token }))
+            setAuth(data)
+        } catch (err) {
+            console.log('auth error', err)
+            this.state = 'error'
+        }
+    })
+
+    @action
+    relogin = userData => {
+        this.authenticated = true
+        this.user = userData.user
+        this.state = 'done'
+        setAuth(userData)
     }
-  })
-
-  @action
-  logout = () => {
-    setAuth({})
-    this.authenticated = false
-  }
-
-  register = flow(function*({ email, password }) {
-    this.state = 'pending'
-    try {
-      const data = yield doJsonRequest({
-        url: REGISTER_URL,
-        method: 'POST',
-        data: { email, password },
-      })
-      console.log('--', data)
-      this.authenticated = true
-      this.user = data.user
-      this.state = 'done'
-      setAuth(data)
-    } catch (err) {
-      console.log('auth error', err)
-      this.state = 'error'
+    logout = () => {
+        setAuth({})
+        this.authenticated = false
+        localStorage.removeItem('bconf')
     }
-  })
 
-  resetPassword = flow(function*({ email }) {
-    this.state = 'pending'
-    try {
-      //todo
-      this.state = 'done'
-    } catch (err) {
-      console.log('auth error', err)
-      this.state = 'error'
-    }
-  })
+    register = flow(function*({ email, name, password }) {
+        this.state = 'pending'
+        try {
+            const data = yield doJsonRequest({
+                url: REGISTER_URL,
+                method: 'POST',
+                data: { email, name, password },
+            })
+            console.log('--', data)
+            this.authenticated = true
+            this.user = data.user
+            this.state = 'done'
+            console.log(data)
+            setAuth(data)
+        } catch (err) {
+            console.log('auth error', err)
+            this.state = 'error'
+        }
+    })
+
+    resetPassword = flow(function*({ email }) {
+        this.state = 'pending'
+        try {
+            //todo
+            this.state = 'done'
+        } catch (err) {
+            console.log('auth error', err)
+            this.state = 'error'
+        }
+    })
 }
